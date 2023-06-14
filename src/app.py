ed21ba0f10b06c89288a4f4e9d717a33831d601f
victorianav <victoria.cnavw@gmail.com>
2023-06-13 23:56:05 -0300

    #6 Commit prueba

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app= Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Taylor Swift, Fearless(TV), Speak Now (TV), Red(TV), 1989, Rep, Folklore, Evermore, Midnights"
+    return "Taylor Swift(TV), Fearless(TV), Speak Now (TV), Red(TV), 1989, Rep(TV), Folklore, Evermore, Midnights"
 
 if __name__ == "__main__": 
     app.run()
