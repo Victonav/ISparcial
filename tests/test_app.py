7b2aca6fe8930ef8755c80471a41e10692b30c89
victorianav <victoria.cnavw@gmail.com>
2023-06-13 23:48:34 -0300

    #5 Commit prueba

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 from app import index
 
 def test_index():
-    assert index()== "Taylor Swift(TV), Fearless(TV), Speak Now (TV), Red(TV), 1989(TV), Rep, Folklore, Evermore, Midnights"
+    assert index()== "Taylor Swift(TV), Fearless(TV), Speak Now (TV), Red(TV), 1989, Rep, Folklore, Evermore, Midnights"
